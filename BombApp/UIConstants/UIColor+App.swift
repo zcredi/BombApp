710f322f11eeec23bb566bfaf4941def6685638d
Vladislav <107634092+zcredi@users.noreply.github.com>
2023-08-07 12:46:37 +0300

    add pattern MVC

---
+++ BombApp/UIConstants/UIColor+App.swift
@@ -0,0 +1,38 @@
+//
+//  UIColor+App.swift
+//  BombApp
+//
+//  Created by Владислав on 07.08.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var blackColor: UIColor {
+        UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1)
+    }
+    
+    static var specialBlackColor: UIColor {
+        UIColor(red: 0.05, green: 0.05, blue: 0.05, alpha: 1.00)
+    }
+    
+    static var purpleColor: UIColor {
+        UIColor(red: 0.55, green: 0.24, blue: 0.85, alpha: 1.00)
+    }
+    
+    static var specialPurpleColor: UIColor {
+        UIColor(red: 0.51, green: 0.19, blue: 0.65, alpha: 1.00)
+    }
+    
+    static var yellowColor: UIColor {
+        UIColor(red: 0.97, green: 0.97, blue: 0.04, alpha: 1.00)
+    }
+    
+    static var specialYellowColor: UIColor {
+        UIColor(red: 0.98, green: 1.00, blue: 0.00, alpha: 1.00)
+    }
+    
+    static var whiteColor: UIColor {
+        UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)
+    }
+}
---
+++ BombApp/UIConstants/UIFont+App.swift
@@ -0,0 +1,44 @@
+//
+//  UIFont+App.swift
+//  BombApp
+//
+//  Created by Владислав on 07.08.2023.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    //Regular
+    static func delaGothicOneRegular48() -> UIFont? {
+        return UIFont.init(name: "DelaGothicOne-Regular", size: 48)
+    }
+    
+    static func delaGothicOneRegular32() -> UIFont? {
+        return UIFont.init(name: "DelaGothicOne-Regular", size: 32)
+    }
+    
+    static func delaGothicOneRegular30() -> UIFont? {
+        return UIFont.init(name: "DelaGothicOne-Regular", size: 30)
+    }
+    
+    static func delaGothicOneRegular28() -> UIFont? {
+        return UIFont.init(name: "DelaGothicOne-Regular", size: 28)
+    }
+    
+    static func delaGothicOneRegular24() -> UIFont? {
+        return UIFont.init(name: "DelaGothicOne-Regular", size: 24)
+    }
+    
+    static func delaGothicOneRegular20() -> UIFont? {
+        return UIFont.init(name: "DelaGothicOne-Regular", size: 20)
+    }
+    
+    static func delaGothicOneRegular16() -> UIFont? {
+        return UIFont.init(name: "DelaGothicOne-Regular", size: 16)
+    }
+    
+    static func delaGothicOneRegular12() -> UIFont? {
+        return UIFont.init(name: "DelaGothicOne-Regular", size: 12)
+    }
+}
