7e74908be557913a8cfea3ebe93029b4bee45551
fullzoom <fullzoom59@gmail.com>
2023-08-09 00:50:14 +0200

    Set avatar in app

--- BombApp/CategoryModule/Model/QuestionBrain.swift
+++ BombApp/CategoryModule/Model/QuestionBrain.swift
@@ -5,12 +5,7 @@ class Quiestion{
     static var array = Array<Array<String>>()
     
     static func generateQuestions(){
-        ["Miscellaneous",
-         "Sport And Hobbies",
-         "About Life",
-         "Celebrities",
-         "Art and Cinema",
-         "Nature"].forEach { type in
+        ["Miscellaneous", "Sport And Hobbies", "About Life", "Celebrities", "Art and Cinema", "Nature"].forEach { type in
             if let fileURL = Bundle.main.url(forResource: type, withExtension: "txt") {
                 if let fileContents = try? String(contentsOf: fileURL) {
                     var appendLast = fileContents.components(separatedBy: "\n")
