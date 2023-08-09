382114d3e17e872f902e6924ffc15ad917b5ea14
Vladislav <107634092+zcredi@users.noreply.github.com>
2023-08-09 10:17:59 +0300

    add

--- BombApp/CategoryModule/Controllers/CategoryViewController.swift
+++ BombApp/CategoryModule/Controllers/CategoryViewController.swift
@@ -6,6 +6,7 @@ class CategoryViewController: UIViewController, MyCollectionViewCellDelegate {
     private let image: [UIImage] = [UIImage(named: "image1")!, UIImage(named: "image2")!, UIImage(named: "image3")!, UIImage(named: "image4")!, UIImage(named: "image5")!, UIImage(named: "image6")!]
     private var selectedItems: [String] = []
     private var isSelected: Bool = false
+    var n = 10
     
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
