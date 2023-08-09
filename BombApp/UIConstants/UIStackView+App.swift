9c5fe8ec7dfaa02ca0238ee6aaf4943123a8ebf0
Vladislav <107634092+zcredi@users.noreply.github.com>
2023-08-09 15:29:14 +0300

    Revert "Merge branch 'develop' into feature/SettingsVC"
    This reverts commit c59675b64d0cef9c1dec5e352ac38be398f3d8c3, reversing
    changes made to 1ca23555e24673995a2c24a008330492a7dd06fc.

---
+++ BombApp/UIConstants/UIStackView+App.swift
@@ -0,0 +1,18 @@
+//
+//  UIStackView.swift
+//  BombApp
+//
+//  Created by Evgenii Mazrukho on 08.08.2023.
+//
+
+import UIKit
+
+extension UIStackView {
+    
+    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.spacing = spacing
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ BombApp/UIConstants/Unique/StopButton.swift
@@ -0,0 +1,27 @@
+//
+//  stopButton.swift
+//  BombApp
+//
+//  Created by Evgenii Mazrukho on 08.08.2023.
+//
+
+import UIKit
+
+class StopButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configure() {
+        setBackgroundImage(UIImage(named: "stopButton"), for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
