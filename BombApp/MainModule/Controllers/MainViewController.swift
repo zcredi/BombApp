b967c0a514a6beabead97baf9f3a04a1c42f04b9
Vladislav <107634092+zcredi@users.noreply.github.com>
2023-08-09 15:29:16 +0300

    Revert "Merge pull request #6 from zcredi/feature/SettingsVC"
    This reverts commit 025589672ad748f40974ece03bc6863315a156c7, reversing
    changes made to 2e1f0d82f49761933c6df608071b14155408c65f.

--- BombApp/MainModule/Controllers/MainViewController.swift
+++ BombApp/MainModule/Controllers/MainViewController.swift
@@ -11,9 +11,7 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        
-    }
+        view.backgroundColor = .red    }
     
 
     /*
